6848bc48a04809da28c623f4512dea0ff852218c
root <root@managehu.ipa.shanghai.nyu.edu>
2023-07-06 16:33:01 +0800

    ad

---
+++ docs/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+make clean
+make html
+grep -rn "Theme by" ./ > delete
+
+
+rm -rf file1
+file="delete"
+while read -r line
+do
+    #echo -e "$line\n"
+    dir=$(echo $line | tr ":" "\n")
+    echo $dir | awk '{print $1}' >> file1
+    #sed -i "s| | |g" $dir | awk '{print $1}'
+done <$file
+
+file1="file1"
+
+while read -r line
+do
+    sed -i 's| | |g' $line 
+done < $file1
+
+
+
+git add .
+git commit -m "ad"
+git push origin master:master
